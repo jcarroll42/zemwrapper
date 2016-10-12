6d692262605175d74e8ca33980e6adb80517dc8c
Josh Carroll <joshcarroll42@gmail.com>
2016-10-11 21:21:31 -0500

    adding mongo

--- app/config/database.js
+++ app/config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://localhost/zemulonDB' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://<dbuser>:<dbpassword>@ds047812.mlab.com:47812/heroku_rrts9hl7' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
