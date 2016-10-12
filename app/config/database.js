d128aceb22788cb9524a22d1881fde990cdd1159
Josh Carroll <joshcarroll42@gmail.com>
2016-10-11 21:33:12 -0500

    adding mongo

--- app/config/database.js
+++ app/config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://<strangegalaxy>:<betelgeuse7>@ds047812.mlab.com:47812/heroku_rrts9hl7' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://strangegalaxy:betelgeuse7@ds047812.mlab.com:47812/heroku_rrts9hl7' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
